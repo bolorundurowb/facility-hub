604bad5f33e340d274828d7ac257b4f94dcd4ba2
Winner-Timothy Bolorunduro <ogatimo@gmail.com>
2023-10-29 02:45:02 +0100

    ft: update dependencies and fix the user model

--- src/models/user.rs
+++ src/models/user.rs
@@ -1,30 +1,32 @@
+use mongodb::bson::{self, oid::ObjectId};
 use serde::{Deserialize, Serialize};
-use mongodb::bson::DateTime;
 use bcrypt::{DEFAULT_COST, hash, verify};
 
-pub mod models {
-    #[derive(Deserialize, Serialize)]
-    struct User {
-        pub first_name: Option<String>,
-        pub last_name: Option<String>,
-        pub email_address: String,
-        pub password_hash: String,
-        created_at: DateTime,
-    }
-
-    impl User {
-        pub fn new(first_name: Option<String>, last_name: Option<String>, email_address: String, password: String) -> Self {
-            let normalized_email = email_address.trim().to_lowercase();
-            let password_hash = hash(password, DEFAULT_COST);
-
-            return User {
-                first_name,
-                last_name,
-                email_address: normalized_email,
-                password_hash,
-                created_at: DateTime(Utc::now())
-            };
-        }
-    }
+#[derive(Deserialize, Serialize)]
+struct User {
+    #[serde(rename = "_id", skip_serializing_if = "Option::is_none")]
+    pub _id: Option<ObjectId>,
+    pub first_name: Option<String>,
+    pub last_name: Option<String>,
+    pub email_address: String,
+    pub password_hash: String,
+    pub created_at: Option<bson::DateTime>,
+    pub updated_at: Option<bson::DateTime>,
 }
 
+impl User {
+    pub fn new(first_name: Option<String>, last_name: Option<String>, email_address: String, password: String) -> Self {
+        let normalized_email = email_address.trim().to_lowercase();
+        let password_hash = hash(password, DEFAULT_COST);
+
+        return User {
+            _id: None,
+            first_name,
+            last_name,
+            email_address: normalized_email,
+            password_hash,
+            created_at: bson::DateTime::now(),
+            updated_at: bson::DateTime::now(),
+        };
+    }
+}

97c2324b33b5bac852fe77d44a6e690441aa5ace
Winner-Timothy Bolorunduro <ogatimo@gmail.com>
2023-12-18 16:36:48 +0100

    ft: add in create facility validator

---
+++ facilityhub/Models/Validators/CreateFacilityReqValidator.cs
@@ -0,0 +1,20 @@
+﻿using FacilityHub.Models.Request;
+using FluentValidation;
+
+namespace FacilityHub.Models.Validators;
+
+public class CreateFacilityReqValidator : AbstractValidator<CreateFacilityReq>
+{
+    public CreateFacilityReqValidator()
+    {
+        RuleFor(req => req.Name)
+            .NotEmpty().WithMessage("Name cannot be empty");
+
+        RuleFor(req => req.Address)
+            .NotEmpty().WithMessage("Address cannot be empty");
+
+        RuleFor(req => req.Location)
+            .SetValidator(new LocationDtoValidator()!) 
+            .When(req => req.Location != null);
+    }
+}

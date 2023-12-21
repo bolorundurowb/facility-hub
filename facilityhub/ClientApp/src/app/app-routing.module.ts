5e3b182b00b815da59b6eec7745bc1ee82005d2e
Winner-Timothy Bolorunduro <ogatimo@gmail.com>
2023-12-21 23:37:03 +0100

    ft: fix routing

--- facilityhub/ClientApp/src/app/app-routing.module.ts
+++ facilityhub/ClientApp/src/app/app-routing.module.ts
@@ -2,13 +2,19 @@ import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
 
 import {HomeComponent} from './home/home.component';
+import {AuthComponent} from "./auth/auth.component";
 
 const routes: Routes = [
   {
     path: '',
     component: HomeComponent,
     pathMatch: 'full'
-  }
+  },
+  {
+    path: 'auth',
+    component: AuthComponent,
+    loadChildren: () => import('./auth/auth.module').then(x => x.AuthModule)
+  },
 ]
 
 @NgModule({

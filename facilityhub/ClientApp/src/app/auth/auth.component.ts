48cd839979b5c35ef0483f446a38c6ca9c65b8f7
Winner-Timothy Bolorunduro <ogatimo@gmail.com>
2023-12-25 20:44:24 +0100

    ft: add in a logo

--- facilityhub/ClientApp/src/app/auth/auth.component.ts
+++ facilityhub/ClientApp/src/app/auth/auth.component.ts
@@ -5,27 +5,14 @@ import { Component } from "@angular/core";
   template: `
     <div class="container">
       <div class="box">
+        <div class="logo">
+          <img src="/assets/logo/main-transparent.svg" />
+        </div>
         <router-outlet></router-outlet>
       </div>
     </div>
   `,
-  styles: `
-    .container {
-      display: flex;
-      justify-content: center;
-      align-items: center;
-      height: 100vh;
-    }
-
-    .box {
-      background-color: #dedede;
-      border-radius: 0.5rem;
-      padding: 1rem 1.5rem;
-      max-width: 400px;
-      width: 100%;
-      border: 2px solid #cacaca;
-    }
-  `
+  styleUrl: './auth.component.scss'
 })
 export class AuthComponent {
 }

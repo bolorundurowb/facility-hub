bcf4eb42ec7b8e8dac0e4373c0768fda6507e638
Winner-Timothy Bolorunduro <ogatimo@gmail.com>
2023-12-30 15:26:35 +0100

    ft: add document table update migration

---
+++ facilityhub/Migrations/20231230141752_TrackExternalIdOnDocumentsTable.cs
@@ -0,0 +1,95 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FacilityHub.Migrations
+{
+    /// <inheritdoc />
+    public partial class TrackExternalIdOnDocumentsTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "FileExt",
+                table: "Document");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Url",
+                table: "Document",
+                type: "character varying(1024)",
+                maxLength: 1024,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "text");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "MimeType",
+                table: "Document",
+                type: "character varying(100)",
+                maxLength: 100,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "text");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "FileName",
+                table: "Document",
+                type: "character varying(256)",
+                maxLength: 256,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "text");
+
+            migrationBuilder.AddColumn<string>(
+                name: "ExternalId",
+                table: "Document",
+                type: "character varying(256)",
+                maxLength: 256,
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ExternalId",
+                table: "Document");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Url",
+                table: "Document",
+                type: "text",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "character varying(1024)",
+                oldMaxLength: 1024);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "MimeType",
+                table: "Document",
+                type: "text",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "character varying(100)",
+                oldMaxLength: 100);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "FileName",
+                table: "Document",
+                type: "text",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "character varying(256)",
+                oldMaxLength: 256);
+
+            migrationBuilder.AddColumn<string>(
+                name: "FileExt",
+                table: "Document",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+        }
+    }
+}

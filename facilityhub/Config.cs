855fd41f6da2d8e3461287461479f152b019fb1e
Winner-Timothy Bolorunduro <ogatimo@gmail.com>
2023-12-30 14:17:16 +0100

    ft: add in request model and validator

--- facilityhub/Config.cs
+++ facilityhub/Config.cs
@@ -11,4 +11,11 @@ public static class Config
     public static string Audience => "facilityhub.africa";
 
     public static string Issuer => "facilityhub.africa";
+    
+    public const long MaxDocumentSize = 10 * 1024 * 1024; // 10MB
+
+    public static readonly string[] AcceptedPhotoFileExtensions = { ".jpg", ".jpeg", ".png", ".svg", ".webp" };
+
+    public static readonly string[] AcceptedDocumentFileExtensions =
+        { ".jpg", ".jpeg", ".png", ".doc", ".docx", ".pdf", ".xls", ".xlsx" };
 }
---
+++ facilityhub/Models/Request/UploadDocumentReq.cs
@@ -0,0 +1,10 @@
+﻿using FacilityHub.Enums;
+
+namespace FacilityHub.Models.Request;
+
+public class UploadDocumentReq
+{
+    public DocumentType DocumentType { get; set; }
+
+    public IFormFile File { get; set; } = null!;
+}

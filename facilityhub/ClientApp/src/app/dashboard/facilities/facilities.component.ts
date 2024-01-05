e660167decc5656b906e6242f31daea4712de78f
Winner-Timothy Bolorunduro <ogatimo@gmail.com>
2024-01-05 20:19:16 +0100

    ft: add facilities scaffold

---
+++ facilityhub/ClientApp/src/app/dashboard/facilities/facilities.component.ts
@@ -0,0 +1,8 @@
+import {Component} from "@angular/core";
+
+@Component({
+  selector: 'fh-facilities',
+  templateUrl: './facilities.component.html',
+  styleUrl: './facilities.component.scss'
+})
+export class FacilitiesComponent {}

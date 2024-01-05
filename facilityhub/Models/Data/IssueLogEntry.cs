24fdac9b96be2380d6bf24450bebb266cc918c0d
Winner-Timothy Bolorunduro <ogatimo@gmail.com>
2024-01-05 16:46:48 +0100

    ft: add issue details and update model

---
+++ facilityhub/Models/Data/IssueLogEntry.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+using FacilityHub.Enums;
+
+namespace FacilityHub.Models.Data;
+
+public class IssueLogEntry
+{
+    public IssueStatus? TransitionedFrom { get; private set; }
+    
+    public IssueStatus TransitionedTo { get; private set; }
+    
+    [StringLength(1024)]
+    public string? Notes { get; private set; }
+
+#pragma warning disable CS8618
+    private IssueLogEntry() { }
+#pragma warning restore CS8618
+
+    public IssueLogEntry(IssueStatus? transitionedFrom, IssueStatus transitionedTo, string? notes)
+    {
+        TransitionedFrom = transitionedFrom;
+        TransitionedTo = transitionedTo;
+        Notes = notes;
+    }
+}
---
+++ facilityhub/Models/Response/IssueRes.cs
@@ -0,0 +1,18 @@
+﻿using FacilityHub.Enums;
+
+namespace FacilityHub.Models.Response;
+
+public class IssueRes
+{
+    public Guid Id { get; set; }
+
+    public string Code { get; set; } = null!;
+
+    public string Title { get; set; } = null!;
+
+    public string Details { get; set; } = null!;
+
+    public IssueStatus Status { get; set; }
+
+    public DateTimeOffset FiledAt { get; set; }
+}

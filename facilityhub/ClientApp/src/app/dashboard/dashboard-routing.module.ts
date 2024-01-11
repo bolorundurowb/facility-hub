28626f31bbf65d7462fb607e0e249dc458172a26
Winner-Timothy Bolorunduro <ogatimo@gmail.com>
2024-01-11 12:14:32 +0100

    ft: add facility details routing logic

--- facilityhub/ClientApp/src/app/dashboard/dashboard-routing.module.ts
+++ facilityhub/ClientApp/src/app/dashboard/dashboard-routing.module.ts
@@ -2,11 +2,26 @@ import { RouterModule, Routes } from '@angular/router';
 import { FacilitiesComponent } from './facilities/facilities.component';
 import { NgModule } from '@angular/core';
 import { HomeComponent } from './home/home.component';
+import { FacilityDetailsComponent } from './facility-details/facility-details.component';
 
 const routes: Routes = [
-  { path: 'facilities', component: FacilitiesComponent, },
-  { path: 'home', component: HomeComponent, pathMatch: 'full' },
-  { path: '**', redirectTo: 'home' }
+  {
+    path: 'facilities',
+    component: FacilitiesComponent,
+  },
+  {
+    path: 'facilities/:facilitiesId',
+    component: FacilityDetailsComponent,
+  },
+  {
+    path: 'home',
+    component: HomeComponent,
+    pathMatch: 'full'
+  },
+  {
+    path: '**',
+    redirectTo: 'home'
+  }
 ];
 
 @NgModule({

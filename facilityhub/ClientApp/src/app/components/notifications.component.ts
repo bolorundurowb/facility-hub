c36e2cba9e22a9ec41dca61b9ef01b8f90e0ebdb
Winner-Timothy Bolorunduro <ogatimo@gmail.com>
2024-01-15 12:32:20 +0100

    ft: finalize notification logic

--- facilityhub/ClientApp/src/app/components/notifications.component.ts
+++ facilityhub/ClientApp/src/app/components/notifications.component.ts
@@ -1,7 +1,7 @@
-import { Component, OnDestroy, OnInit, Output } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { cilBell } from '@coreui/icons';
 import { Colors } from '@coreui/angular';
-import { NotificationService } from '../services/notification.service';
+import { NotificationService } from '../services';
 
 export enum ToastType {
   SUCCESS,
@@ -19,13 +19,12 @@ export interface NotificationData {
   template: `
     <c-toaster placement="top-end" position="absolute" style="margin-right: 0.5rem; margin-top: 0.5rem;">
       <ng-container *ngFor="let toast of toasts">
-        <c-toast [autohide]="true" [fade]="true" [visible]="true" [color]="getColorForType(toast.type)"
-                 (visibleChange)="toastDismissed(toast)">
+        <c-toast [autohide]="true" [fade]="true" [visible]="true" [color]="getColorForType(toast.type)">
           <c-toast-header>
             <svg [cIcon]="icons.cilBell"></svg>
             &nbsp; {{ toast.title ?? 'Notification' }}
           </c-toast-header>
-          <c-toast-body>{{toast.message}}</c-toast-body>
+          <c-toast-body>{{ toast.message }}</c-toast-body>
         </c-toast>
       </ng-container>
     </c-toaster>
@@ -33,19 +32,15 @@ export interface NotificationData {
 })
 export class NotificationsComponent implements OnInit, OnDestroy {
   icons = { cilBell };
-
-  toasts: NotificationData[] = [
-    { message: 'Hello from the other side' }
-  ];
+  toasts: NotificationData[] = [];
 
   constructor(private notificationService: NotificationService) {
   }
 
   ngOnInit() {
     this.notificationService.notificationEmitter.subscribe((notification) => {
-      console.log('Notification received', notification);
       this.toasts.push(notification);
-    })
+    });
   }
 
   getColorForType(type?: ToastType): Colors | undefined {
@@ -60,12 +55,7 @@ export class NotificationsComponent implements OnInit, OnDestroy {
     return undefined;
   }
 
-  toastDismissed(event: any) {
-    // this.toasts = this.toasts.filter(x => x!== event);
-  }
-
   ngOnDestroy() {
-    console.warn('Unsubscribing from the notification service');
     this.notificationService.notificationEmitter?.unsubscribe();
   }
 }

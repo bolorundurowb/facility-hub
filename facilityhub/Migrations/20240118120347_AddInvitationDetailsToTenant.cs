c93320e890b8eb33029e85eca2c84c2845d35049
Winner-Timothy Bolorunduro <ogatimo@gmail.com>
2024-01-18 13:06:35 +0100

    ft: add in migration

---
+++ facilityhub/Migrations/20240118120347_AddInvitationDetailsToTenant.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FacilityHub.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddInvitationDetailsToTenant : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "EmailAddress",
+                table: "Tenant",
+                type: "character varying(256)",
+                maxLength: 256,
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Name",
+                table: "Tenant",
+                type: "character varying(512)",
+                maxLength: 512,
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "PhoneNumber",
+                table: "Tenant",
+                type: "character varying(256)",
+                maxLength: 256,
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "EmailAddress",
+                table: "Tenant");
+
+            migrationBuilder.DropColumn(
+                name: "Name",
+                table: "Tenant");
+
+            migrationBuilder.DropColumn(
+                name: "PhoneNumber",
+                table: "Tenant");
+        }
+    }
+}

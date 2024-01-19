f88649e61d57664930bd38c9108fed6043c66c58
Winner-Timothy Bolorunduro <ogatimo@gmail.com>
2024-01-19 13:08:30 +0100

    ft: add request logic

--- facilityhub/ClientApp/src/app/auth/forgot-password/forgot.password.component.ts
+++ facilityhub/ClientApp/src/app/auth/forgot-password/forgot.password.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { Title } from '@angular/platform-browser';
-import { AuthService } from '../../services';
+import { AuthService, NotificationService } from '../../services';
 import { Router } from '@angular/router';
 
 interface ForgotPasswordPayload {
@@ -20,6 +20,49 @@ export class ForgotPasswordComponent {
   errorMessage: string | undefined;
   payload: ForgotPasswordPayload = {};
 
-  constructor(title: Title, private authService: AuthService, private router: Router) {
+  constructor(title: Title, private authService: AuthService, private router: Router,
+              private notificationService: NotificationService) {
+  }
+
+  async requestReset() {
+    this.isBusy = true;
+
+    try {
+      const hasError = this.validatePayload();
+
+      if (!hasError) {
+        await this.authService.requestReset(this.payload);
+        this.notificationService.showSuccess("A reset code has been dispatched to your account");
+        await this.router.navigate([ 'auth', 'reset-password' ]);
+      }
+    } catch (e: any) {
+      this.errorMessage = e.message;
+      this.hasError = true;
+    } finally {
+      this.isBusy = false;
+    }
+  }
+
+  dismissError(): void {
+    this.hasError = false;
+    this.errorMessage = undefined;
+  }
+
+  private validatePayload(): boolean {
+    let message = null;
+    if (!this.payload.emailAddress) {
+      message = 'An email address is required';
+    } else if (!this.emailRegex.test(this.payload.emailAddress)) {
+      message = 'Email address is invalid';
+    }
+
+    if (message) {
+      this.errorMessage = message;
+      this.hasError = true;
+    } else {
+      this.dismissError();
+    }
+
+    return this.hasError;
   }
 }

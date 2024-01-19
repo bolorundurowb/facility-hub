ab5aece734659881f8332d69ee8fe3661a45ce73
Winner-Timothy Bolorunduro <ogatimo@gmail.com>
2024-01-19 11:58:17 +0100

    ft: add in migrations

---
+++ facilityhub/Migrations/20240119105740_AddResetCodeToUsers.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FacilityHub.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddResetCodeToUsers : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ResetCode",
+                table: "Users",
+                type: "text",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ResetCode",
+                table: "Users");
+        }
+    }
+}

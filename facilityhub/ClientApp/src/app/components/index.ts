9791eac36a90991a2fdb0475384b5b21c7d3d9bc
Winner-Timothy Bolorunduro <ogatimo@gmail.com>
2024-01-25 10:57:25 +0100

    ft: add in widget component scaffold

--- facilityhub/ClientApp/src/app/components/index.ts
+++ facilityhub/ClientApp/src/app/components/index.ts
@@ -2,3 +2,4 @@ export * from './enums';
 export * from './types';
 
 export * from './notifications.component';
+export * from './widget.component';
---
+++ facilityhub/ClientApp/src/app/components/widget.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'fh-widget',
+  template: `<div>Hello</div>`
+})
+export class WidgetComponent {
+
+}

a617d0e9fa237ade870848797d06e324afba3152
Winner-Timothy Bolorunduro <ogatimo@gmail.com>
2024-02-01 12:35:47 +0100

    ft: add in users controller and logic

---
+++ facilityhub/Controllers/UsersController.cs
@@ -0,0 +1,29 @@
+﻿using FacilityHub.Extensions;
+using FacilityHub.Models.Request;
+using FacilityHub.Models.Response;
+using FacilityHub.Services.Interfaces;
+using MapsterMapper;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FacilityHub.Controllers;
+
+public class UsersController : ApiController
+{
+    private readonly IUserService _userService;
+
+    public UsersController(IMapper mapper, IUserService userService) : base(mapper) => _userService = userService;
+
+    [HttpGet("current")]
+    [ProducesResponseType(typeof(UserRes), 200)]
+    [ProducesResponseType(typeof(GenericRes), 401)]
+    public async Task<IActionResult> GetCurrentUser()
+    {
+        var userId = User.GetCallerId();
+        var user = await _userService.FindById(userId);
+
+        if (user == null)
+            return Unauthorized("User account does not exist");
+
+        return Ok(Mapper.Map<UserRes>(user));
+    }
+}

665fdf7be8fa480f2ae80c0ae1f333b8c5430c56
Winner-Timothy Bolorunduro <ogatimo@gmail.com>
2024-02-01 12:54:00 +0100

    ft: general clean up of c# code

--- facilityhub/Models/Email/EmailRecipient.cs
+++ facilityhub/Models/Email/EmailRecipient.cs
@@ -3,9 +3,9 @@ namespace FacilityHub.Models.Email;
 public class EmailRecipient
 {
     public string Name { get; }
-    
+
     public string Email { get; }
-    
+
     public EmailRecipient(string name, string email)
     {
         Name = name;

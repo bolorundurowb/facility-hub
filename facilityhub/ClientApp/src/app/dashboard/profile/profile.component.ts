6867368331f495e63775c0fbfb16f3f2801b2a0e
Winner-Timothy Bolorunduro <ogatimo@gmail.com>
2024-02-03 18:36:13 +0100

    ft: add profile page scaffold

---
+++ facilityhub/ClientApp/src/app/dashboard/profile/profile.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'fh-dashboard-profile',
+  templateUrl: './profile.component.html',
+  styleUrl: './profile.component.scss'
+})
+export class ProfileComponent {
+
+}

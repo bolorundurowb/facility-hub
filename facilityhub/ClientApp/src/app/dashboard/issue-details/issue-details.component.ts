afaf5827dee9e7524cc4aae5380a38f053791383
Winner-Timothy Bolorunduro <ogatimo@gmail.com>
2024-02-18 01:32:31 +0100

    ft: add in the document upload UI

--- facilityhub/ClientApp/src/app/dashboard/issue-details/issue-details.component.ts
+++ facilityhub/ClientApp/src/app/dashboard/issue-details/issue-details.component.ts
@@ -1,11 +1,12 @@
 import { Component, OnInit } from '@angular/core';
-import { DocumentRes, IssueRes } from '../../components';
+import { DocumentRes, DocumentUploadPayload, IssueRes } from '../../components';
 import { Title } from '@angular/platform-browser';
 import { ActivatedRoute } from '@angular/router';
 import { Location } from '@angular/common';
-import { AuthService, FileDownloadService, IssuesService } from '../../services';
+import { AuthService, FileDownloadService, IssuesService, NotificationService } from '../../services';
 import { cilArrowLeft, cilCloudDownload, cilCloudUpload, cilNoteAdd, cilTrash, cilUserPlus } from '@coreui/icons';
 import { getIssueColour } from '../../utils';
+import { HttpEventType } from '@angular/common/http';
 
 @Component({
   selector: 'fh-dashboard-issue-details',
@@ -24,8 +25,13 @@ export  class IssueDetailsComponent implements OnInit {
   hasError = false;
   errorMessage?: string;
 
+  isNewDocModalVisible = false;
+  isUploadingDoc = false;
+  newDocPayload: DocumentUploadPayload = {};
+
   constructor(title: Title,  private route: ActivatedRoute, private location: Location, private authService: AuthService,
-              private downloadService: FileDownloadService, private issueService: IssuesService) {
+              private downloadService: FileDownloadService, private issueService: IssuesService,
+              private notificationService: NotificationService) {
     title.setTitle('Issue Details | Facility Hub');
   }
 
@@ -50,4 +56,42 @@ export  class IssueDetailsComponent implements OnInit {
   }
 
   protected readonly getIssueColour = getIssueColour;
+
+  showDocumentModal() {
+    this.isNewDocModalVisible = true;
+  }
+
+  dismissDocumentModal() {
+    this.isNewDocModalVisible = false;
+    this.newDocPayload = {};
+  }
+
+  documentSelected(event: any) {
+    this.newDocPayload.file = event.target.files[0];
+  }
+
+  uploadDocument() {
+    this.isUploadingDoc = true;
+    this.hasError = false;
+
+    this.issueService.uploadDocument(this.issueId!, this.newDocPayload)
+      .subscribe({
+        next: (event) => {
+          if (event.type === HttpEventType.Response) {
+            const document = event.body as DocumentRes;
+            console.log(document);
+            this.documents.unshift(document);
+
+            this.dismissDocumentModal();
+
+            this.notificationService.showSuccess('Document uploaded successfully');
+            this.isUploadingDoc = false;
+          }
+        }, error: (err) => {
+          this.errorMessage = err as string;
+          this.hasError = true;
+          this.isUploadingDoc = false;
+        }
+      });
+  }
 }

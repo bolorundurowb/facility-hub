7ff55c6c78ac9df7a4872fcbb24f81efcae80a38
Winner-Timothy Bolorunduro <ogatimo@gmail.com>
2024-04-13 19:46:43 +0100

    ft: add in slightly better mobile styling

--- facilityhub/ClientApp/src/app/dashboard/dashboard.component.ts
+++ facilityhub/ClientApp/src/app/dashboard/dashboard.component.ts
@@ -4,13 +4,12 @@ import { INavData } from '@coreui/angular';
 @Component({
   selector: 'fh-dashboard',
   template: `
-    <c-sidebar visible [narrow]="isCompact">
+    <c-sidebar visible>
       <c-sidebar-brand routerLink="/">
         <img class="brand-logo" src="/assets/logo/white-transparent.svg" style="margin-left: -3rem;"/>
         <span class="align-middle ms-2">Facility Hub</span>
       </c-sidebar-brand>
       <c-sidebar-nav [navItems]="sidebarLinks" dropdownMode="close"></c-sidebar-nav>
-      <c-sidebar-toggler (click)="toggleSidebar()"></c-sidebar-toggler>
     </c-sidebar>
 
     <div class="sidebar-content">
@@ -22,6 +21,12 @@ import { INavData } from '@coreui/angular';
       margin-left: 16rem;
       padding: 2rem;
       height: 100vh;
+    }
+
+    @media only screen and (max-width: 600px) {
+      .sidebar-content {
+        margin-left: 0;
+      }
     }
   `
 })
@@ -48,10 +53,4 @@ export class DashboardComponent {
       iconComponent: { name: 'cil-user' }
     },
   ];
-
-  isCompact = false;
-
-  toggleSidebar() {
-    this.isCompact = !this.isCompact;
-  }
 }
